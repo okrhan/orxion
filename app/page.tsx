0119f56a17175f63dffddf5f5e7d63074c0ad983
Orkhan MAMMADOV <mmmdv.orkhan@gmail.com>
2026-04-06 23:18:58 +0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <html>
+      <head>
+        <meta httpEquiv="refresh" content="0; url=/orxion.html" />
+      </head>
+      <body>
+        <p>Loading...</p>
+      </body>
+    </html>
+  );
+}
